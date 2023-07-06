be2419bf4f6188c51a3f132e16deb13f1c1876d8
theogee <theophilusgee17@gmail.com>
2023-07-06 13:35:38 +0200

    feat: added student count badge

--- src/routes/admin/StudentTable.jsx
+++ src/routes/admin/StudentTable.jsx
@@ -37,31 +37,43 @@ export default function StudentTable({ data, currentPage, setCurrentPage }) {
                 </table>
             </div>
             {data && (
-                <div className="absolute right-0 -top-20 flex items-center gap-3">
-                    <div
-                        className="px-3 border-2 rounded-md bg-white flex items-center py-2 hover:cursor-pointer"
-                        onClick={() => {
-                            if (currentPage > 1)
-                                setCurrentPage((oldState) => --oldState);
-                        }}
-                    >
-                        <ArrowIcon
-                            w={6}
-                            h={10}
-                            className="fill-slate-400 flex"
-                        />
+                <div className="absolute right-0 -top-20 flex items-center gap-10">
+                    <div className="px-3 py-2 rounded-md bg-slate-100 text-xs font-extrabold text-slate-400">
+                        <p>count: {data.totalStudent}</p>
                     </div>
-                    <p className="font-bold text-xs">
-                        {currentPage} of {Math.ceil(data.totalStudent / 20)}
-                    </p>
-                    <div
-                        className="px-3 border-2 rounded-md bg-white rotate-180 flex items-center py-2 hover:cursor-pointer"
-                        onClick={() => {
-                            if (currentPage < Math.ceil(data.totalStudent / 20))
-                                setCurrentPage((oldState) => ++oldState);
-                        }}
-                    >
-                        <ArrowIcon w={6} h={10} className="fill-slate-400" />
+                    <div className="flex items-center gap-3">
+                        <div
+                            className="px-3 border-2 rounded-md bg-white flex items-center py-2 hover:cursor-pointer"
+                            onClick={() => {
+                                if (currentPage > 1)
+                                    setCurrentPage((oldState) => --oldState);
+                            }}
+                        >
+                            <ArrowIcon
+                                w={6}
+                                h={10}
+                                className="fill-slate-400 flex"
+                            />
+                        </div>
+                        <p className="font-bold text-xs">
+                            {currentPage} of {Math.ceil(data.totalStudent / 20)}
+                        </p>
+                        <div
+                            className="px-3 border-2 rounded-md bg-white rotate-180 flex items-center py-2 hover:cursor-pointer"
+                            onClick={() => {
+                                if (
+                                    currentPage <
+                                    Math.ceil(data.totalStudent / 20)
+                                )
+                                    setCurrentPage((oldState) => ++oldState);
+                            }}
+                        >
+                            <ArrowIcon
+                                w={6}
+                                h={10}
+                                className="fill-slate-400"
+                            />
+                        </div>
                     </div>
                 </div>
             )}
